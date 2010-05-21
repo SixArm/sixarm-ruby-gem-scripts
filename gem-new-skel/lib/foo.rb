5d53324e27befa846441dc100190b9f9ff0692f0
Joel Parker Henderson <joel@sixarm.com>
2010-05-20 20:54:40 -0700

    first commit

---
+++ gem-new-skel/lib/foo.rb
@@ -0,0 +1,19 @@
+# = {camelize}
+#
+# Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+# Copyright:: Copyright (c) 2009-2010 Joel Parker Henderson
+# License:: CreativeCommons License, Non-commercial Share Alike
+# License:: LGPL, GNU Lesser General Public License
+#
+#
+# ==Example
+#
+##
+
+class {camelize}
+
+  #
+  def 
+  end
+
+end
