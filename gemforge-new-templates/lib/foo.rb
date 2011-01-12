bb2dee384dd990649729094ad04f07f5f9b6384e
SixArm <sixarm@sixarm.com>
2011-01-12 03:43:34 -0700

    $1

---
+++ gemforge-new-templates/lib/foo.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+=begin rdoc
+
+= SixArm Ruby Gem »
+
+Author:: Joel Parker Henderson, joel@sixarm.com
+Copyright:: Copyright (c) 2009-2010 Joel Parker Henderson
+License:: Your choice of BSD, MIT, LGPL, or CreativeCommons Non-commercial Share Alike
+
+Summary line...
+
+Description line...
+
+@example
+
+
+=end
+
+class {camelize}
+
+  #
+  def 
+  end
+
+end
---
+++ gemforge-new-templates/test/foo_test.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+require '{name}'
+
+class {camelize}Test < Test::Unit::TestCase
+
+ # 
+ def test_
+  assert()
+ end
+
+end
+
